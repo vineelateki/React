0bcb4d8d20f39b28789bbb97884b8eeaa07a0562
vineelateki <vineelateki666@gmail.com>
2025-03-04 12:50:32 +0530

    commit

---
+++ theme-app/src/ThemeContext.js
@@ -0,0 +1,18 @@
+import React, { createContext, useState } from "react";
+
+export const ThemeContext = createContext();
+
+export function ThemeProvider({ children }) {
+
+  const [theme, setTheme] = useState("light");
+
+  return (
+<ThemeContext.Provider value={{ theme, setTheme }}>
+
+      {children}
+</ThemeContext.Provider>
+
+  );
+
+}
+ 
