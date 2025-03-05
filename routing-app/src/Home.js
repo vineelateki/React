ccb196f2e7b7791ac8b0abe8b51dbf3227f59fa5
vineelateki <vineelateki666@gmail.com>
2025-03-05 17:49:34 +0530

    commit

---
+++ routing-app/src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h2>🍔 Welcome to Food Delight!</h2>
+      <p>Your favorite spot for delicious meals. Check out our menu!</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ routing-app/src/Menu.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Menu() {
+  return (
+    <div>
+      <h2>🥗 Our Delicious Menu</h2>
+      <ul>
+        <li>🍕 Pizza - $12</li>
+        <li>🍔 Burger - $8</li>
+        <li>🌮 Tacos - $10</li>
+        <li>🥗 Salad - $7</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Menu;
